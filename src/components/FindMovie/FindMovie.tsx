e52018c369f585e9619c3718f084e9699976cadb
Iryna <irakogut23190@gmail.com>
2025-05-19 20:02:31 +0300

    solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -3,7 +3,7 @@ import './FindMovie.scss';
 import { MovieCard } from '../MovieCard';
 import { getMovie } from '../../api';
 import { MovieData } from '../../types/MovieData';
-import { ResponseError } from '../../types/ReponseError';
+import { ResponseError } from '../../types/ResponseError';
 import classNames from 'classnames';
 
 type Props = {
